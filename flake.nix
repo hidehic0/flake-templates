727f9287b61fc844f230c8c166295ff4adfeeed6
hidehic0 <hidehico.ooba@gmail.com>
2026-02-28 17:58:50 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -5,11 +5,21 @@
   };
   outputs =
     inputs@{ nixpkgs, flake-parts, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
     flake-parts.lib.mkFlake { inherit inputs; } {
+      inherit systems;
       flake = {
         templates = {
           default = {
             path = ./default;
+            description = "default";
           };
         };
       };
